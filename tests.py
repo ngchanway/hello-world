9106c0deee77784099b7be8fee7911dad5c5d6dc
Ng Chan Way <chanwayng@u.nus.edu>
2020-06-15 19:02:09 +0800

    fix: Fix assert

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from hello import app
 with app.test_client() as c:
     response = c.get('/')
-    assertEqual(response.status_code, 200)
-    assertEqual(response.data, b'Hello World!')
+    assert(response.status_code == 200)
+    assert(response.data == b'Hello World!')
